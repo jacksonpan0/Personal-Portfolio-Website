ce5d652a509f10c7e5f29e494365e6ee0b324b4e
pnjnlng <pan.jackson1@gmail.com>
2023-11-16 12:48:59 -0500

    Added button selector for projects in different languages

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 document.getElementById("contact-button").addEventListener("click", function() {
     window.location.href = "mailto:jpan23@u.rochester.edu";
 });
+function showAllProjects() {
+    showProjectsByLanguage('');
+}
+
+function showProjects(language) {
+    showProjectsByLanguage(language);
+}
+
+function showProjectsByLanguage(language) {
+    const allProjects = document.getElementById('all-projects').children;
+
+    for (const project of allProjects) {
+        const projectLanguages = project.getAttribute('data-languages');
+
+        if (!language || projectLanguages.includes(language)) {
+            project.style.display = 'inline';
+        } else {
+            project.style.display = 'none';
+        }
+    }
+}
