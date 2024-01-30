80b1fdbdbdbce0110a3ad508cdc1a9a05bc10994
pnjnlng <pan.jackson1@gmail.com>
2024-01-30 10:36:14 -0500

    Changed functions syntax

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 document.getElementById("contact-button").addEventListener("click", function() {
     window.location.href = "mailto:jpan23@u.rochester.edu";
 });
-function showAllProjects() {
+
+const showAllProjects = () => {
     showProjectsByLanguage('');
 }
 
-function showProjects(language) {
+const showProjects = (language) => {
     showProjectsByLanguage(language);
 }
 
-function showProjectsByLanguage(language) {
+const showProjectsByLanguage = (language) => {
     const allProjects = document.getElementById('all-projects').children;
-    for (const project of allProjects) {
+    for(const project of allProjects) {
         const projectLanguages = project.getAttribute('data-languages');
-        if (!language || projectLanguages.includes(language)) {
+        if(!language || projectLanguages.includes(language)) {
             project.style.display = 'flex';
-        } else {
+        }
+        else {
             project.style.display = 'none';
         }
     }
 }
+
